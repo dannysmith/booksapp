9b74509e2869dd6c24730cf1bc193043d10e7f84
Danny Smith <hi@danny.is>
2018-11-20 03:20:04 +0000

    Add function to call Airtable

---
+++ functions/getBooks.js
@@ -0,0 +1,17 @@
+const AIRTABLE_API_KEY = process.env.AIRTABLE_API_KEY
+
+exports.handler = function(event, context, callback) {
+  fetch('https://api.airtable.com/v0/appB4V75drkeQ0rJc/Books', {
+    headers: {
+      'content-type': 'application/json',
+      Authorization: 'Bearer ' + AIRTABLE_API_KEY,
+    },
+  })
+    .then((res) => res.json())
+    .then((json) => {
+      callback(null, {
+        statusCode: 200,
+        body: json,
+      })
+    })
+}
