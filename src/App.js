472d34f7529877a414f645b8f63d012b0dfd3cac
Danny Smith <hi@danny.is>
2018-11-20 00:11:34 +0000

    Get books from API and set as state

--- src/App.js
+++ src/App.js
@@ -2,6 +2,28 @@ import React, { Component } from 'react'
 import './App.scss'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      books: [],
+      isLoaded: false,
+    }
+  }
+
+  componentDidMount() {
+    fetch(
+      'https://gist.githubusercontent.com/dannysmith/0e203810e2561d235fad1175487b43c3/raw/5e4bb34d44c00f2ad2a5d0d1fdcb93fc3364a0d2/books.json'
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        this.setState({
+          isLoaded: true,
+          books: json,
+        })
+      })
+  }
+
   render() {
     return (
       <div className="app">
