a5f2cb33db1a9c5931a39d5dbb87ad79785f9bf8
Danny Smith <hi@danny.is>
2018-11-20 14:42:28 +0000

    Lots of config

---
+++ src/functions/getBooks.js
@@ -0,0 +1,19 @@
+import fetch from 'node-fetch'
+
+const AIRTABLE_API_KEY = process.env.AIRTABLE_API_KEY
+
+exports.handler = function(event, context, callback) {
+  fetch('https://api.airtable.com/v0/appB4V75drkeQ0rJc/Books', {
+    headers: {
+      'content-type': 'application/json',
+      Authorization: 'Bearer ' + AIRTABLE_API_KEY,
+    },
+  })
+    .then((res) => res.json())
+    .then((json) => {
+      callback(null, {
+        statusCode: 200,
+        body: json,
+      })
+    })
+}
